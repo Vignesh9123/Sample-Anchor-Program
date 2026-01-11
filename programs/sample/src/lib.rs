bf7817dccbc3a47d77c4bcbf96605a6cdbd90e6e
Vignesh D <vignesh.d9123@gmail.com>
2026-01-11 13:12:35 +0000

    Add Vault PDA methods and tests

--- programs/sample/src/lib.rs
+++ programs/sample/src/lib.rs
@@ -23,6 +23,33 @@ pub mod sample {
         msg!("Transferred {} lamports", amount);
         Ok(())
     }
+
+    pub fn initialize_vault(ctx:Context<InitializeVault>) -> Result<()>{
+        msg!("Vault initialized at {} with bump {}", ctx.accounts.vault.key(),ctx.bumps.vault);
+        ctx.accounts.vault.authority = ctx.accounts.user.key();
+        Ok(())
+    }
+
+    pub fn transfer_to_vault(ctx:Context<TransferToVault>, amount: u64) -> Result<()>{
+        system_program::transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                system_program::Transfer {
+                    from: ctx.accounts.user.to_account_info(),
+                    to: ctx.accounts.vault.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
+        Ok(())
+    }
+
+    pub fn transfer_to_user_from_vault(ctx: Context<TransferToUserFromVault>, amount: u64)->Result<()>{
+        **ctx.accounts.vault.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **ctx.accounts.user.to_account_info().try_borrow_mut_lamports()? += amount;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -34,4 +61,55 @@ pub struct Transfer<'info> {
     pub to: SystemAccount<'info>,
 
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeVault<'info>{
+    #[account(
+        init,
+        payer = user,
+        space = 8 + size_of::<VaultStruct>(),
+        seeds = [b"vault", user.key().as_ref()],
+        bump
+    )]
+    pub vault: Account<'info,VaultStruct>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct TransferToVault<'info>{
+    #[account(
+        mut,
+        seeds = [b"vault",user.key().as_ref()],
+        bump
+    )]
+    pub vault: Account<'info,VaultStruct>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+
+}
+
+#[derive(Accounts)]
+pub struct TransferToUserFromVault<'info>{
+    #[account(
+        mut,
+        seeds = [b"vault",user.key().as_ref()],
+        bump
+    )]
+    pub vault: Account<'info,VaultStruct>,
+    #[account(mut)]
+    pub user: SystemAccount<'info>,
+    pub system_program: Program<'info, System>
+
+}
+
+
+#[account]
+pub struct VaultStruct {
+    pub authority: Pubkey,
 }
--- tests/sample.ts
+++ tests/sample.ts
@@ -2,7 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Sample } from "../target/types/sample";
 import { expect } from "chai";
-
+import { PublicKey, Connection } from "@solana/web3.js";
 
 describe("sample", () => {
   // Configure the client to use the local cluster.
@@ -11,7 +11,7 @@ describe("sample", () => {
 
   const program = anchor.workspace.sample as Program<Sample>;
 
-  it("Is initialized!", async () => {
+  it("Is transferred!", async () => {
     // Add your test here.
     const from = anchor.web3.Keypair.generate();
     const to = anchor.web3.Keypair.generate();
@@ -32,4 +32,80 @@ describe("sample", () => {
     const toBalance = await provider.connection.getBalance(to.publicKey);
     expect(toBalance).to.be.equal(amount.toNumber());
   });
+
+  it("is_vault_initialized", async()=>{
+    const user = anchor.web3.Keypair.generate();
+    const sig = await provider.connection.requestAirdrop(user.publicKey, anchor.web3.LAMPORTS_PER_SOL);
+    await provider.connection.confirmTransaction(sig);
+    const tx = await program.methods.initializeVault()
+    .accounts({
+      user:user.publicKey
+    })
+    .signers([user])
+    .rpc()
+    const txDetails = await provider.connection.getTransaction(tx, {
+      maxSupportedTransactionVersion: 0,
+      commitment: "confirmed",
+    });
+    const logs = txDetails?.meta?.logMessages || null
+    console.log("logs", logs)
+    if(logs){
+      console.log("Logs", logs)
+    }
+  })
+
+  it("is_transferring_to_vault", async()=>{
+    const user = anchor.web3.Keypair.generate();
+    const sig = await provider.connection.requestAirdrop(user.publicKey, anchor.web3.LAMPORTS_PER_SOL);
+    await provider.connection.confirmTransaction(sig);
+    const tx = await program.methods.initializeVault()
+    .accounts({
+      user:user.publicKey
+    })
+    .signers([user])
+    .rpc()
+    let transfer_amount = new anchor.BN(anchor.web3.LAMPORTS_PER_SOL/2)
+
+    const tx2 = await program.methods.transferToVault(transfer_amount)
+    .accounts({
+      user: user.publicKey
+    })
+    .signers([user])
+    .rpc()
+    console.log("Transaction: ",tx2)
+    const seeds = [Buffer.from("vault"), user.publicKey.toBuffer()];
+    const [pdaPublicKey, bumpSeed] = PublicKey.findProgramAddressSync(seeds, program.programId) 
+    const decodedAccount = await program.account["vaultStruct"].fetch(pdaPublicKey);
+    console.log("Decoded acc",decodedAccount.authority.toBase58())
+    expect(decodedAccount.authority.toBase58()).to.be.equal(user.publicKey.toBase58())
+  })
+
+  it("is_transferring_to_user_from_vault", async()=>{
+    const user = anchor.web3.Keypair.generate();
+    const sig = await provider.connection.requestAirdrop(user.publicKey, anchor.web3.LAMPORTS_PER_SOL);
+    await provider.connection.confirmTransaction(sig);
+    const tx = await program.methods.initializeVault()
+    .accounts({
+      user:user.publicKey
+    })
+    .signers([user])
+    .rpc()
+    const transfer_amount = new anchor.BN(anchor.web3.LAMPORTS_PER_SOL/2)
+
+    const tx2 = await program.methods.transferToVault(transfer_amount)
+    .accounts({
+      user: user.publicKey
+    })
+    .signers([user])
+    .rpc()
+    console.log("Transaction: ",tx2)
+    const transfer_back_amount = new anchor.BN(transfer_amount.toNumber() - 0.1 * anchor.web3.LAMPORTS_PER_SOL )
+    const tx3 = await program.methods.transferToUserFromVault(transfer_back_amount)
+    .accounts({
+      user: user.publicKey
+    })
+    .rpc()
+    console.log("Transaction 3: ", tx3)
+  })
+  
 });
